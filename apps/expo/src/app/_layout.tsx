4bc3f0e01fb8edf078565f0db4f863f66afa2a99
Guilherme S <guilhermes@Guilhermes-MacBook-Air.local>
2024-11-26 12:19:33 -0300

    gluestack on expo

--- apps/expo/src/app/_layout.tsx
+++ apps/expo/src/app/_layout.tsx
@@ -2,6 +2,7 @@ import "@bacons/text-decoder/install";
 
 import { Stack } from "expo-router";
 import { StatusBar } from "expo-status-bar";
+import { GluestackUIProvider } from "@/components/ui/gluestack-ui-provider";
 import { useColorScheme } from "nativewind";
 
 import { TRPCProvider } from "~/utils/api";
@@ -11,24 +12,26 @@ import "../styles.css";
 // This is the main layout of the app
 // It wraps your pages with the providers they need
 export default function RootLayout() {
-  const { colorScheme } = useColorScheme();
+  // const { colorScheme } = useColorScheme();
   return (
-    <TRPCProvider>
-      {/*
-          The Stack component displays the current page.
-          It also allows you to configure your screens 
-        */}
-      <Stack
-        screenOptions={{
-          headerStyle: {
-            backgroundColor: "#f472b6",
-          },
-          contentStyle: {
-            backgroundColor: colorScheme == "dark" ? "#09090B" : "#FFFFFF",
-          },
-        }}
-      />
-      <StatusBar />
-    </TRPCProvider>
+    <GluestackUIProvider mode="light">
+      <TRPCProvider>
+        {/*
+            The Stack component displays the current page.
+            It also allows you to configure your screens 
+          */}
+        <Stack
+          screenOptions={{
+            headerStyle: {
+              backgroundColor: "#f472b6",
+            },
+            // contentStyle: {
+            //   backgroundColor: colorScheme == "dark" ? "#09090B" : "#FFFFFF",
+            // },
+          }}
+        />
+        <StatusBar />
+      </TRPCProvider>
+    </GluestackUIProvider>
   );
 }
--- apps/expo/tailwind.config.ts
+++ apps/expo/tailwind.config.ts
@@ -5,6 +5,10 @@ import nativewind from "nativewind/preset";
 import baseConfig from "@acme/tailwind-config/native";
 
 export default {
-  content: ["./src/**/*.{ts,tsx}"],
+  content: [
+    "index.{tsx,jsx,ts,js}",
+    "src/**/*.{tsx,jsx,ts,js}",
+    "components/**/*.{tsx,jsx,ts,js}",
+  ],
   presets: [baseConfig, nativewind],
 } satisfies Config;
