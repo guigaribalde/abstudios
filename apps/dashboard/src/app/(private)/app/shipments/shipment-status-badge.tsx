ed39ea7a6049fab904e1ec00953a5c25c752955d
dc143c <v3nus.milo@gmail.com>
2024-12-16 12:22:08 -0300

    chore: removing comments

--- apps/dashboard/src/app/(private)/app/shipments/shipment-status-badge.tsx
+++ apps/dashboard/src/app/(private)/app/shipments/shipment-status-badge.tsx
@@ -1,22 +1,30 @@
-import { Badge } from '@/components/ui/badge';
-import { Check, X } from 'lucide-react';
+import { Badge } from "@/components/ui/badge";
+import { Check, X } from "lucide-react";
 
-type BadgeVariants = 'success' | 'error' | 'default' | 'secondary' | 'destructive' | 'outline' | null | undefined;
+type BadgeVariants =
+  | "success"
+  | "error"
+  | "default"
+  | "secondary"
+  | "destructive"
+  | "outline"
+  | null
+  | undefined;
 const mapStatus: Record<string, BadgeVariants> = {
-  // pending: 'warning',
-  // shipped: 'information',
-  pending: 'success',
-  shipped: 'success',
-  delivered: 'success',
-  cancelled: 'error',
+  pending: "success",
+  shipped: "success",
+  delivered: "success",
+  cancelled: "error",
 };
 
-const ShipmentStatusBadge = (
-  { status }: { status: 'pending' | 'shipped' | 'delivered' | 'cancelled' },
-) => {
+const ShipmentStatusBadge = ({
+  status,
+}: {
+  status: "pending" | "shipped" | "delivered" | "cancelled";
+}) => {
   return (
     <Badge variant={mapStatus[status]}>
-      {mapStatus[status] === 'success' ? <Check size={12} /> : <X size={12} />}
+      {mapStatus[status] === "success" ? <Check size={12} /> : <X size={12} />}
       {status}
     </Badge>
   );
