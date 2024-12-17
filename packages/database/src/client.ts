8b6985c723925aa0d0a9777401552b8a77411cb1
guigaribalde <me@guiga.dev>
2024-12-17 12:39:08 -0300

    fix(database): type error

--- packages/database/src/client.ts
+++ packages/database/src/client.ts
@@ -15,14 +15,16 @@
 import type { PostgresJsDatabase } from 'drizzle-orm/postgres-js';
 import { sql } from '@vercel/postgres';
 import { drizzle as drizzlePostgres } from 'drizzle-orm/postgres-js';
-import { drizzle as drizzleVercel } from 'drizzle-orm/vercel-postgres';
+import { drizzle as drizzleVercel, type VercelPgDatabase } from 'drizzle-orm/vercel-postgres';
 import postgres from 'postgres';
 import * as schema from './schema';
 
 const isLocal = process.env.NODE_ENV === 'development';
 const localUrl = 'postgresql://postgres:postgres@127.0.0.1:54322/postgres';
 
-export const db: PostgresJsDatabase<typeof schema> = isLocal
+type DBType = PostgresJsDatabase<typeof schema> | VercelPgDatabase<typeof schema>;
+
+export const db: DBType = isLocal
   ? drizzlePostgres(postgres(localUrl), {
     schema,
     casing: 'snake_case',
